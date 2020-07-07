3986f9da9e96a57410cb253f5824f714a6e62457
Gungoguma <khh9550@naver.com>
2020-07-07 14:17:42 +0900

    feat: 클릭시 취소선

--- Todolist/Todolist.js
+++ Todolist/Todolist.js
@@ -1,30 +1,41 @@
 let index = 1;
 
 const makeTodo = (todo) => {
-    const todolistDom = document.querySelector(".list");
-    const template = `
-    <div id="char-${index++}">
+  const todolistDom = document.querySelector(".list");
+  const template = `
         <div class="header">
-            <input class="do" type="checkbox">
+            <input id="do-${index}" type="checkbox">
             <span class="name">${todo}</span>
         </div>
-    </div>
     `;
-    todolistDom.innerHTML += template;
+  const newTodo = document.createElement("div");
+  newTodo.id = `char-${index}`;
+  newTodo.innerHTML = template;
+
+  todolistDom.appendChild(newTodo);
+
+  const doDom = newTodo.querySelector(`#do-${index}`);
+  doDom.addEventListener("click", () => {
+    const content = newTodo.querySelector(".name").style.textDecoration;
+    if (content !== "line-through") {
+      newTodo.querySelector(".name").style.textDecoration = "line-through";
+      newTodo.querySelector(".name").style.opacity = "0.7";
+      return;
+    }
+    newTodo.querySelector(".name").style.textDecoration = "";
+    newTodo.querySelector(".name").style.opacity = "1";
+  });
+
+  index++;
 };
 
 const todoDom = document.querySelector(".todo");
-const doDom = document.querySelector(".do");
 
 const sendTodo = () => {
-    makeTodo(todoDom.value);
-    todoDom.value = "";
+  makeTodo(todoDom.value);
+  todoDom.value = "";
 };
 
 todoDom.addEventListener("keydown", (e) => {
-    if(e.key === "Enter") sendTodo();
+  if (e.key === "Enter") sendTodo();
 });
-
-doDom.addEventListener("click", () => {
-    
-})
